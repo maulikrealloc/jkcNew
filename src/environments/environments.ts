3e796adda3b58c398e70f72e51b29464cd175623
maulikrealloc <maulikrealloc@gmail.com>
2024-09-03 12:13:13 +0530

    angular firebase setup

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+
+    firebaseConfig : {
+        apiKey: "AIzaSyB-dj7ja0OizFa8GQJcibDE4Q_43XBsF9I",
+        authDomain: "jkc-new.firebaseapp.com",
+        projectId: "jkc-new",
+        storageBucket: "jkc-new.appspot.com",
+        messagingSenderId: "678726642490",
+        appId: "1:678726642490:web:0577796605ab575d363e03",
+        measurementId: "G-VLV46MG7DC"
+      }
+  };
+  

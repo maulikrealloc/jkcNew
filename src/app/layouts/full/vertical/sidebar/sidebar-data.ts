9235f98f710ff7f78ad38cd5282389eb7220d8c2
maulikrealloc <maulikrealloc@gmail.com>
2024-09-03 13:51:20 +0530

    test firebase deploy

--- src/app/layouts/full/vertical/sidebar/sidebar-data.ts
+++ src/app/layouts/full/vertical/sidebar/sidebar-data.ts
@@ -5,7 +5,7 @@ export const navItems: NavItem[] = [
     navCap: 'Home',
   },
   {
-    displayName: 'Dashboard',
+    displayName: 'Dashboard test',
     iconName: 'aperture',
     route: '/dashboards/dashboard1',
   },
